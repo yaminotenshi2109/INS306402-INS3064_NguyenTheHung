22b7ae32793afc24359cbb37157b8848b68bd8ca
hungnguyen <lifeisajourney2109@gmail.com>
2026-02-02 21:59:13 +0700

    New HW

---
+++ S2/input.php
@@ -0,0 +1,26 @@
+<?php
+ 
+// Input section
+// $a = 10
+$a = (int)readline('Enter an integer: ');
+
+// $b = 9.78
+$b = (float)readline('Enter a floating'
+            . ' point number: ');
+
+// Entered integer is 10 and
+// entered float is 9.78
+echo "Entered integer is " . $a 
+    . " and entered float is " . $b;
+?>
+
+
+<?php
+
+// For input
+// Hello World
+$a = readline('Enter a string: ');
+
+// For output
+echo $a;    
+?>
